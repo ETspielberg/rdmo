5ecb762cf96e4740823dae24de3c257355308868
Jochen Klar <jklar@aip.de>
2016-12-14 16:33:53 +0100

    refactor domain django app

--- apps/conditions/models.py
+++ apps/conditions/models.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.core.validators import RegexValidator
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
