360d7a2b8a4446e743e4c504253bc07d9bf7cf03
Jochen Klar <jklar@aip.de>
2016-12-15 17:49:16 +0100

    refactor tasks django app

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -14,17 +14,56 @@ from apps.conditions.models import Condition
 @python_2_unicode_compatible
 class Task(TranslationMixin, models.Model):
 
-    attribute = models.ForeignKey(Attribute, blank=True, null=True, on_delete=models.SET_NULL, related_name='+')
+    identifier = models.SlugField(
+        max_length=256, unique=True,
+        verbose_name=_('Identifier'),
+        help_text=_('The unambiguous internal identifier of this task.')
+    )
+    uri = models.URLField(
+        max_length=256, blank=True, null=True,
+        verbose_name=_('URI'),
+        help_text=_('The Uniform Resource Identifier of this task.')
+    )
+    comment = models.TextField(
+        blank=True, null=True,
+        verbose_name=_('Comment'),
+        help_text=_('Additional information about this task.')
+    )
+    attribute = models.ForeignKey(
+        Attribute, blank=True, null=True, on_delete=models.SET_NULL, related_name='+',
+        verbose_name=_('Attribute'),
+        help_text=_('The attribute this task is refering to.')
+    )
+    time_period = models.DurationField(
+        verbose_name=_('Time period'),
+        help_text=_('The the time period after this task becomes active.')
+    )
+    title_en = models.CharField(
+        max_length=256,
+        verbose_name=_('Title (en)'),
+        help_text=_('The English title for this task.')
+    )
+    title_de = models.CharField(
+        max_length=256,
+        verbose_name=_('Title (de)'),
+        help_text=_('The German title for this task.')
+    )
 
-    time_period = models.DurationField()
-
-    title_en = models.CharField(max_length=256)
-    title_de = models.CharField(max_length=256)
-
-    text_en = models.CharField(max_length=256)
-    text_de = models.CharField(max_length=256)
-
-    conditions = models.ManyToManyField(Condition, blank=True)
+    text_en = models.CharField(
+        max_length=256,
+        verbose_name=_('Text (en)'),
+        help_text=_('The English text for this task.')
+    )
+    text_de = models.CharField(
+        max_length=256,
+        verbose_name=_('Text (de)'),
+        help_text=_('The German text for this task.')
+    )
+    conditions = models.ManyToManyField(
+        Condition, blank=True,
+        verbose_name=_('Conditions'),
+        help_text=_('The list of conditions evaluated for this task.')
+    )
 
     class Meta:
         ordering = ('attribute',)
